502fef50ff8d4bcd9d0499ec40afb9957ca0a409
Web Dev Atanu <atanushil358@gmail.com>
2024-09-04 20:48:09 +0530

    update in responsive in about section

--- client/src/components/common/AboutUs.jsx
+++ client/src/components/common/AboutUs.jsx
@@ -5,45 +5,50 @@ const AboutUs = () => {
   return (
     <section
       id="about-section"
-      className="py-16 max-w-screen-xl mx-auto grid lg:grid-cols-4 
-      lg:grid-rows-7 px-8  grid-cols-1 grid-rows-9"
+      className="py-16 max-w-screen-xl mx-auto md:grid md:grid-cols-4 
+      md:grid-rows-6 px-8 flex flex-col "
     >
-      <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2  
-      drop-shadow-[0_3px_3px_rgba(199,87,224,0.8)] caret-transparent
-       lg:text-start text-stone-400  text-8xl whitespace-nowrap text-center">
+      <div
+        className="md:col-span-2 md:col-start-1 
+        drop-shadow-[0_3px_3px_rgba(199,87,224,0.8)] text-stone-400 
+        lg:text-8xl sm:text-6xl md:text-6xl text-4xl flex items-center justify-center"
+      >
         Our Mission
       </div>
 
-      <div className="lg:col-span-2 lg:row-span-5 lg:col-start-1 lg:row-start-3 row-span-4
-       bg-slate/10 flex justify-center items-center">
+      <div
+        className="md:col-span-2 md:row-span-5 md:col-start-1 md:row-start-2
+        bg-slate/10 flex justify-center items-center"
+      >
         <div className="relative group">
           <img
             src={Logo}
             alt="logo"
-            className=" object-cover 
-            group-hover:drop-shadow-[0_3px_3px_rgba(199,87,224,0.8)] transition-transform 
-            duration-500 ease-in-out transform group-hover:scale-110 "
+            className="object-cover group-hover:drop-shadow-[0_3px_3px_rgba(199,87,224,0.8)] 
+            transition-transform duration-500 ease-in-out transform group-hover:scale-110"
           />
         </div>
       </div>
 
       <div
-        className="lg:col-span-2 lg:row-span-7 lg:col-start-3 lg:row-start-1
-        row-span-4 row-start-6 lg:px-0 px-8 h-fit py-4 lg:h-full lg:py-0
-         bg-slate/20 lg:grid grid-cols-1 grid-rows-7"
+        className="md:col-span-2 md:row-span-7 md:col-start-3  
+         lg:px-0 sm:px-8 py-4  bg-slate/20 md:grid md:grid-cols-1 md:grid-rows-7"
       >
-        <div className="row-span-5 row-start-3 px-4 text-stone-300  ">
-          At <span className="text-animate">SkillHub </span>, our mission is to
-          revolutionize the way developers present their skills and connect with
-          job opportunities across the globe. We are dedicated to providing a
-          platform that not only highlights your unique talents but also
-          verifies them through advanced blockchain technology, ensuring that
-          your credentials are both authentic and credible. By bridging the gap
-          between exceptional talent and top-notch opportunities, we strive to
-          empower developers to achieve their career goals, accelerate their
-          growth, and thrive in a competitive job market. Our commitment is to
-          foster a supportive community where professionals can unlock their
-          full potential and secure their ideal roles.
+        <div className="md:row-span-6 md:row-start-2 sm:px-4 px-2 text-stone-300 h-full">
+          <p className="sm:text-xl text-sm">
+            At <span className="text-animate">SkillHub</span>, our mission is
+            to revolutionize the way developers present their skills and connect
+            with job opportunities across the globe. We are dedicated to
+            providing a platform that not only highlights your unique talents
+            but also verifies them through advanced blockchain technology,
+            ensuring that your credentials are both authentic and credible. By
+            bridging the gap between exceptional talent and top-notch
+            opportunities, we strive to empower developers to achieve their
+            career goals, accelerate their growth, and thrive in a competitive
+            job market. Our commitment is to foster a supportive community where
+            professionals can unlock their full potential and secure their ideal
+            roles.
+          </p>
           <br />
           <a href="/about-us">
             <span className="lg:top-6 top-0 text-xl relative font-medium hover:text-orange-500 hover:underline">
--- client/src/components/common/ContactUs.jsx
+++ client/src/components/common/ContactUs.jsx
@@ -1,78 +1,13 @@
 import React from "react";
-import { Banner, heroImage1 } from "../../utils";
 
 const ContactUs = () => {
   return (
-    <div
-      className="grid grid-cols-6  grid-rows-3 mx-auto max-w-screen-xl px-8  py-16"
-      id="contant-section"
-    >
-      <div className="col-span-6 row-span-2 flex items-center bg-blend-multiply h-32 object-cover" 
-      style={{backgroundImage:`url(${Banner})`}}>
-        <p className="text-8xl text-white">Contact Us</p></div>
-      <div className="col-span-2 row-span-3 col-start-5 row-start-3 group  bg-white/20 backdrop-blur-sm">
-        <img
-          src={heroImage1}
-          alt="logo"
-          className=" object-cover 
-            group-hover:drop-shadow-[0_3px_3px_rgba(199,87,224,0.8)] transition-transform 
-            duration-500 ease-in-out transform "
-        />
-      </div>
-      <div className="col-span-2 row-span-3 col-start-5 row-start-6">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58548.29511198467!2d87.69214374407845!3d23.486838535589012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f9cef43bf77503%3A0x57e86383904410c7!2sGuskhara%2C%20West%20Bengal%20713128!5e0!3m2!1sen!2sin!4v1725402617941!5m2!1sen!2sin"
-          className="w-full h-full"
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        />
-      </div>
-      <div className="col-span-4 row-span-3 col-start-1 row-start-6">
-        <form className="grid grid-cols-4 grid-rows-5 gap-2 py- bg-white/20 px-10">
-          <div className="col-span-4 text-4xl  text-stone-300 my-auto">
-            Leave a Reply
-          </div>
-          <div className="col-span-2 row-start-2 flex flex-col">
-            <label htmlFor="name" className="text-white">Full Name</label>
-            <input type="text" name="" id="" className="h-12 px-2"/>
-          </div>
-          <div className="col-span-2 col-start-3 row-start-2 flex flex-col">
-            <label htmlFor="email" className="text-white">Email</label>
-            <input type="text" name="" id="" className="h-12 px-2" />
-          </div>
-          <div className="col-span-4 row-span-2 row-start-3">
-            <textarea name="message" id="" className="w-full h-full"></textarea>
-          </div>
-          <button className="col-span-2 col-start-2 row-start-5 btn-primary h-fit py-2 mt-4">Submit</button>
-        </form>
-      </div>
-      <div className="col-start-1 row-start-3 grid place-content-center border-b-2">
-        <p>Address</p>
-      </div>
-      <div className="col-span-3 col-start-2 row-start-3 border-b-2 grid place-content-center">
-        <p>Guskhara West Bengal India 713128</p>
-      </div>
-      <div className="col-start-1 row-start-4  grid place-content-center border-b-2">
-        <p>Get in Touch</p>
-      </div>
-      <div className="col-span-3 col-start-2 row-start-4  grid place-content-center border-b-2">
-        <p className="flex gap-8">
-          <span>+91 9832118794</span>
-          <span>atanushil358@gmail.com</span>
-        </p>
-      </div>
-      <div className="col-start-1 row-start-5  grid place-content-center border-b-2">
-        <p>Hours</p>
-      </div>
-      <div className="col-span-3 col-start-2 row-start-5 grid grid-cols-2 grid-rows-3 px-16 border-b-2 py-2 ">
-        <div>Mon to Fri :</div>
-        <div>9:30 am - 6:00 pm</div>
-        <div>Sat :</div>
-        <div>10:00 am - 4:00 pm </div>
-        <div>Sun :</div>
-        <div>12:00 am - 4:00 pm </div>
-      </div>
+    <div className="grid grid-cols-7 grid-rows-7 gap-4 max-w-screen-lg bg-red-400">
+      <div className="col-span-7">1</div>
+      <div className="col-span-4 row-span-3 row-start-2">2</div>
+      <div className="col-span-4 row-span-3 col-start-1 row-start-5">3</div>
+      <div className="col-span-3 row-span-3 col-start-5 row-start-2">4</div>
+      <div className="col-span-3 row-span-3 col-start-5 row-start-5">5</div>
     </div>
   );
 };
